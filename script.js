1cff2bda798522b703a666b812b20080b4aefd77
Luigi Rosatone <166885110+luigirosatone@users.noreply.github.com>
2024-04-13 10:31:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $('button').on('mouseover',function() {
   $(this).css({
 button.style.left = `${Math.ceil(Math.random() * 90)}%`;
-    button.style.top = `${Math.ceil(Math.random() * 90)}%`;
+    button.style.top = `${Math.ceil(Math.random() * 30)}%`;
   });
 });
