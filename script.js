75a8e9c243fdcd816a9ee08b697b4786cfa39049
Luigi Rosatone <166885110+luigirosatone@users.noreply.github.com>
2024-04-15 08:37:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $('button').on('mouseover',function() {
   $(this).css({
-    top: (Math.random()*50)+'50%',
-    left: (Math.random()*30)+'50%' 
+    top: (Math.random()*50)+'90%',
+    left: (Math.random()*30)+'90%' 
   });
 });
